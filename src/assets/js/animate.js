8723244ac1b5567043cf238f86855f2906dd022a
B.Huskie <1665370368@qq.com>
2019-03-16 17:24:59 +0800

    增加首页小图标淡出隐藏示

--- src/assets/js/animate.js
+++ src/assets/js/animate.js
@@ -2,8 +2,12 @@ define(["jquery"], function($) {
   "use strict";
   $(document).scroll(function() {
     var scrollTop = $(this).scrollTop();
+    document.title = scrollTop;
+    var $locateIcon = $(".explain-wrapper span");
     if (scrollTop >= 900) {
       $(".explain-wrapper span").fadeIn("slow");
+    } else {
+      $(".explain-wrapper span").fadeOut("slow");
     }
   });
 });
